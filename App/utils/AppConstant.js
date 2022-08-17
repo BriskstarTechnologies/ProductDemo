2690dc6531dde5e31362de64f18407d6de1910e0
Jigar Rughani <jigar.rughani@briskstar.com>
2022-08-17 12:04:26 +0530

    - remove extra code

--- App/utils/AppConstant.js
+++ App/utils/AppConstant.js
@@ -1,7 +1,3 @@
-
-//const BASE_URL="http://72.167.223.170:1001/api/";
-
-
 const API_ITEMS_LIST = "https://reqres.in/api/users?paget=2";
 
 const API_PRODUCT_LIST="https://fakestoreapi.com/products";
@@ -12,7 +8,4 @@ const APP_NAME="Product Demo";
 export  default AppConstant ={
 
     APP_NAME,API_ITEMS_LIST,API_PRODUCT_LIST
-
-    
-    
 }
